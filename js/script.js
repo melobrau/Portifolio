f177eece8206da73de0d9d7090b8d95d5acd7415
Mateus Melo <melo.artecontato@gmail.com>
2022-09-07 01:17:33 -0300

    Primeiro commit do Portifolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+document.body.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        window.location.replace("https://www.youtube.com/");
+    }
+});
